ef5ca4c2384de9e49cf798f49134069158f47463
Kragoth <argdaniel.larrea@gmail.com>
2014-04-17 02:47:17 -0300

    asdasdasljdkasjdlkasjdasdasdkasljdaslkd

---
+++ probandoSarasa/sarasa.c
@@ -0,0 +1,21 @@
+/*
+ * sarasa.c
+ *
+ *  Created on: 17/04/2014
+ *      Author: utnso
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+	char* a= "sarasa";
+
+	printf("%s",a);
+
+	return 0;
+
+}
